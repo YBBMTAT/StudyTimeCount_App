ac79a0a79d6f765fd23d30083884c8df1154d3a9
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2024-01-20 23:22:51 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#index"
+  root to: "timers#index"
   resources :timers, only: [:index]
+
+  post '/timers/save', to: 'timers#save'
+
 end
