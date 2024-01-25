d78972fff5562183b9c37fe4b1033600ae2c7e09
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2024-01-25 20:52:12 +0900

    グラフ表示(試行錯誤)

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'webpacker', '~> 5.0'
 end
 
 group :development do
@@ -76,3 +77,6 @@ group :production do
 end
 
 gem 'devise'
+gem 'chartkick'
+gem 'groupdate'
+gem 'pry'
--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -5,9 +5,11 @@ class TimersController < ApplicationController
   end
 
   def show
+    #累積時間の取得
     @cumulative_time = format_duration(current_user.timers.sum(:duration_seconds))
     @weekly_time = format_duration(current_user.timers.where(created_at: 1.week.ago..Time.now).sum(:duration_seconds))
     @month_time = format_duration(current_user.timers.where(created_at: 1.month.ago..Time.now).sum(:duration_seconds))
+    @weekly_data = weekly_data(current_user)
   end
   
   def save
@@ -24,5 +26,14 @@ private
 
   def format_duration(seconds)
     Time.at(seconds).utc.strftime("%H時間%M分%S秒")
+  end
+
+  def weekly_data(user)
+    weekly_data = user.timers
+      .where(created_at: 1.week.ago..Time.now)
+      .group_by_day(:created_at)
+      .sum(:duration_seconds)
+
+      weekly_data.map { |day, duration| [day, duration] }
   end
 end
